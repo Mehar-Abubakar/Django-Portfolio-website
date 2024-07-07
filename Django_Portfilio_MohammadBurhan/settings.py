d19860ecd9ae75279f5f3f76b24e589c5632b2a9
Asad136 <106371078+Asad136@users.noreply.github.com>
2024-07-07 19:04:02 +0500

    comit

--- Django_Portfilio_MohammadBurhan/settings.py
+++ Django_Portfilio_MohammadBurhan/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-85t%)xy5tujn&^s3t2(9$c261e^%o)_9d(sk0d78ku#i^vxhj0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -119,8 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfileS_build","static")
 
 
 
