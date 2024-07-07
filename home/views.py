e8827db684116a239e8bb3d7be6b90a88928c109
Asad136 <106371078+Asad136@users.noreply.github.com>
2024-07-07 14:49:17 +0500

    added

--- home/views.py
+++ home/views.py
@@ -1,32 +1,18 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
+from django.shortcuts import render
+from .models import Contact
 
 def home(request):
-    #contact form database
-    # if request.method == 'POST':
-    #     name == request.POST['name']
-    #     email == request.POST['email']
-    #     subject == request.POST['subject']
-    #     message == request.POST['message']
-    #     contact = models.Home(name=name, email=email, subject=subject, message=message)
-    #     contact.save()
     return render(request, 'home.html')
-
 
 def project(request):
     return render(request, 'project.html')
 
-
 def contact(request):
-    #contact form database
     if request.method == "POST":
-        name == request.POST['name']
-        email == request.POST['email']
-        subject == request.POST['subject']
-        message == request.POST['message']
-        contact = models.Home(name=name, email=email, subject=subject, message=message)
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        contact = Contact(name=name, email=email, subject=subject, message=message)
         contact.save()
     return render(request, 'home.html')
-
